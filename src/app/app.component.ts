a392cee4150048febbb683c35af6440b77fd71e6
joshmorrison <josh.morrison1999@gmail.com>
2023-01-30 11:48:19 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-xss';
+  title = 'Angular XSS';
+  userInput: SafeHtml = "";
+
+  constructor(protected sanitizer: DomSanitizer) {}
+
+  getValue(val:any){
+    this.userInput = this.sanitizer.bypassSecurityTrustHtml(val)
+  }
 }
